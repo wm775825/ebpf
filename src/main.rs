a6b8b70593ef2c1e22f3b7da65d3a14870f8c551
wm775825 <wm775825@mail.ustc.edu.cn>
2022-01-23 00:01:46 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+use futures::stream::StreamExt;
+use std::{ffi::CStr, ptr};
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
 
 use redbpf::load::Loader;
 
-use futures::stream::StreamExt;
-use std::{ffi::CStr, ptr};
-
 use probes::openmonitor::OpenPath;
+
+fn probe_code() -> &'static [u8] {
+    include_bytes!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/target/bpf/programs/openmonitor/openmonitor.elf"
+    ))
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
@@ -25,23 +31,12 @@ async fn main() {
     while let Some((map_name, events)) = loaded.events.next().await {
         if map_name == "OPEN_PATHS" {
             for event in events {
-                let open_path = unsafe {
-                    ptr::read(event.as_ptr() as *const OpenPath)
-                };
+                let open_path = unsafe { ptr::read(event.as_ptr() as *const OpenPath) };
                 unsafe {
                     let cfilename = CStr::from_ptr(open_path.filename.as_ptr() as *const _);
                     println!("{}", cfilename.to_string_lossy());
-                }
+                };
             }
         }
     }
-}
-
-fn probe_code() -> &'static [u8] {
-    include_bytes!(
-        concat!(
-        env!("CARGO_MANIFEST_DIR"),
-        "/target/bpf/programs/openmonitor/openmonitor.elf"
-        )
-    )
 }
