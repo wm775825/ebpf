32430636e741b43636d9b9cc1faa6fe2fb1e19e9
wm775825 <wm775825@mail.ustc.edu.cn>
2022-01-22 22:07:19 +0800

    1

--- probes/src/openmonitor/main.rs
+++ probes/src/openmonitor/main.rs
@@ -1,9 +1,9 @@
 #![no_std]
 #![no_main]
-use cty::*;
+use probes::openmonitor::*;
 
 // use one of the preludes
-// use redbpf_probes::kprobe::prelude::*;
+use redbpf_probes::kprobe::prelude::*;
 // use redbpf_probes::xdp::prelude::*;
 // use redbpf_probes::socket_filter::prelude::*;
 // use redbpf_probes::tc::prelude::*;
@@ -32,3 +32,23 @@ program!(0xFFFFFFFE, "GPL");
 //   };
 //   unsafe { syscall_events.insert(regs.ctx, &event) };
 // }
+
+#[map]
+static mut OPEN_PATHS: PerfMap<OpenPath> = PerfMap::with_max_entries(1024);
+
+#[kprobe]
+fn do_sys_openat2(regs: Registers) {
+    let mut path = OpenPath::default();
+    unsafe {
+        let filename = regs.param2() as *const u8;
+        if bpf_probe_read_str(
+            path.filename.as_mut_str() as *mut _,
+            path.filename.len() as u32,
+            filename as *const _,
+        ) <= 0 {
+            bpf_trace_printk(b"error on bpf_probe_read_str\0");
+            return;
+        }
+        OPEN_PATHS.insert(regs.ctx, &path);
+    }
+}
--- probes/src/openmonitor/mod.rs
+++ probes/src/openmonitor/mod.rs
@@ -1,6 +1,3 @@
-
-use cty::*;
-
 // This is where you should define the types shared by the kernel and user
 // space, eg:
 //
@@ -10,3 +7,20 @@ use cty::*;
 //     pub pid: u64,
 //     ...
 // }
+
+pub const PATHLEN: usize = 256;
+
+#[repr(C)]
+#[derive(Debug, Clone)]
+pub struct OpenPath {
+    pub filename: [u8; PATHLEN]
+}
+
+impl Default for OpenPath {
+    fn default() -> OpenPath {
+        OpenPath {
+            filename: [0; PATHLEN],
+        }
+    }
+}
+
